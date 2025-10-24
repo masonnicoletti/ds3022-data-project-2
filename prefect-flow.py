fe45fc52715b91f43c062ce3fbbdfe8d1ae43bb8
masonnicoletti <mason.f.nicoletti@gmail.com>
2025-10-24 00:44:25 -0400

    Prefect test

--- prefect-flow.py
+++ prefect-flow.py
@@ -1,1 +1,13 @@
 # prefect flow goes here
+from prefect import task, flow
+
+@task
+def start():
+    print("Hello World")
+
+@flow
+def start_flow():
+    start()
+
+if __name__ == "__main__":
+    start_flow()
