dfdf068682031dcbda2aa40747f94163f764bcbc
masonnicoletti <mason.f.nicoletti@gmail.com>
2025-10-29 15:08:34 -0400

    Airflow structural revision

--- airflow/dags/airflow-dag.py
+++ airflow/dags/airflow-dag.py
@@ -9,34 +9,41 @@ submission_queue = "https://sqs.us-east-1.amazonaws.com/440848399208/dp2-submit"
 submission_message = "Data Project 2 Submission"
 platform = "airflow"
 
-def create_api_url(api_endpoint, computing_id):
-    # Create API Endpoint URL
-    api_url = api_endpoint + computing_id
-    return api_url
-
-def get_queue_url(ti):
-    #logger = get_run_logger()
-    api_url = ti.xcom_pull(task_ids="create_api_url")
-    try:
-        # Request Queue URL
-        payload = requests.post(api_url).json()
-        queue_url = payload['sqs_url']
-        #logger.info("Retrieved SQS queue URL")
-    
-    except Exception as e:
-        #logger.error(f"Error retrieving SQS queue URL: {e}")
-        raise e
-    
-    return queue_url
-
-
-with DAG(
+@dag(
     dag_id='dp2_quote_assembler',
     start_date=datetime.datetime(2025, 10, 29),
     schedule="@hourly",
     catchup=False
-) as dag:
+)
+def quote_assembler():
 
+    @task
+    def create_api_url(api_endpoint, computing_id):
+        # Create API Endpoint URL
+        api_url = api_endpoint + computing_id
+        return api_url
+    
+    @task
+    def get_queue_url(api_url):
+        #logger = get_run_logger()
+        try:
+            # Request Queue URL
+            payload = requests.post(api_url).json()
+            queue_url = payload['sqs_url']
+            #logger.info("Retrieved SQS queue URL")
+        
+        except Exception as e:
+            #logger.error(f"Error retrieving SQS queue URL: {e}")
+            raise e
+        
+        return queue_url
+
+
+    api_url = create_api_url(api_endpoint, computing_id)
+    get_queue_url(api_url)
+    
+    
+    '''
     create_url = PythonOperator(
         task_id="create_api_url",
         python_callable=create_api_url,
@@ -47,5 +54,9 @@ with DAG(
        task_id="get_queue_url",
         python_callable=get_queue_url
     )
-    
+
     create_url >> get_queue
+    '''
+
+
+quote_assembler()
